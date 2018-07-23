025f7fa0acfb44523ed3341b832b1c0e17914dcf
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 16:12:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
-    if Item.all
+    if Item.all.size > 0
     Item.all.each do |item|
       if req.path == "/item/#{item.name}"
       @returned = resp.write "#{item.price}"
