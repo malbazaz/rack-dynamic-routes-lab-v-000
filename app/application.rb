35e8e3e3af172c2c552ab6f44edbd0c4378a392b
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 16:01:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,9 @@
+require 'pry'
 class Application
 
 def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
-  @@items = []
-  if req.path.include?"/items/"
     Item.all.each do |item|
       if req.path == "/item/#{item.name}"
         resp.write "#{item.price}"
@@ -14,6 +13,6 @@ def call(env)
     resp.write "Item not found"
     resp.status = 404
   end
-end
+
 
 end
