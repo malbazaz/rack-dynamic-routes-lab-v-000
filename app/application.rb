c14079a9d86f26d80f7e88d6ee43cd682b522062
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 16:16:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,18 +4,19 @@ class Application
 def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
-
-    if Item.all.size > 0
-    Item.all.each do |item|
-      if req.path == "/item/#{item.name}"
-      @returned = resp.write "#{item.price}"
+  @@items = []
+    if req.path.include?("/item/")
+      @@items = Item.all
+      @@items.each do |item|
+        if req.path == "/item/#{item.name}"
+          @returned = resp.write "#{item.price}"
+        end
       end
     end
-  end
     #binding.pry
     if !@returned
       resp.write "Route not found"
       resp.status = 404
     end
-  end
+
 end
