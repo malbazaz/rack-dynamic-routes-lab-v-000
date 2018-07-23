d4ebd2a5ba117ab7a744a5bec086f2cd498499c1
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 15:56:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,12 @@
 class Application
 
-
-
-end 
+def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+  @@items = []
+  if req.path.include?"/items/"
+    Item.all.each do |item|
+      @@items << item.name
+      if req.path == "/item/#{item.name}"
+        resp.write "#{item.price}"
+end
